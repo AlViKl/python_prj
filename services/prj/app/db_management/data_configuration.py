4a1c7a99339f5bfb86b400c463d4ce650c56f900
whoami <whoami>
2022-11-28 21:21:52 +0000

    additional minor fixes

--- services/prj/app/db_management/data_configuration.py
+++ services/prj/app/db_management/data_configuration.py
@@ -4,19 +4,16 @@ from sqlalchemy_utils import database_exists, create_database, drop_database
 import os
 
 URI = "postgresql://*:*@localhost:*/*"
-DB_ECHO = "False"
+DB_ECHO = False
 
 # To separate in to functions
 if os.getenv('URI'):
     URI = os.environ['URI']
-if os.getenv('DB_ECHO'):
-    DB_ECHO = True if os.environ['DB_ECHO'] == 'True' else False
-else:
-    pass
 
 
-Engine = create_engine(URI, echo=DB_ECHO)
+Engine = create_engine(URI, echo=bool(DB_ECHO))
 Base = declarative_base()
+
 
 def db_create(engine=Engine):
     if not database_exists(Engine.url):
@@ -24,11 +21,12 @@ def db_create(engine=Engine):
     else:
         pass
 
+
 Session = sessionmaker(bind=Engine)
+
 
 def db_drop(engine=Engine):
     if database_exists(engine.url):
         drop_database(engine.url)
     else:
         pass
-
